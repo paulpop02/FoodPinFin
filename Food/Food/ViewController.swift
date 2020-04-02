ddc28046d1c29ba02f9c7998426be4dcb5521aed
Paul Pop <ppc.paul@yahoo.com>
2020-04-02 22:09:26 +0300

    first

---
+++ Food/Food/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food
+//
+//  Created by Paul Pop on 02/04/2020.
+//  Copyright © 2020 Paul Pop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
