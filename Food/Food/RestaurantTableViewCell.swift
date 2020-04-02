c51a94e5c85201c01afdde027d701b0975119400
Paul Pop <ppc.paul@yahoo.com>
2020-04-02 23:04:06 +0300

    First list

---
+++ Food/Food/RestaurantTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantTableViewCell.swift
+//  Food
+//
+//  Created by Paul Pop on 02/04/2020.
+//  Copyright © 2020 Paul Pop. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView! {
+        didSet{
+            thumbnailImageView.layer.cornerRadius = thumbnailImageView.bounds.height / 2
+            thumbnailImageView.clipsToBounds = true
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
